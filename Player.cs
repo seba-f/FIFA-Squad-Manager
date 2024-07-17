9c4b3c34972a1520172424445f8fed149735529b
ab3s <mariussienna@gmail.com>
2024-07-17 23:26:34 +0300

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFA_Squad_Manager
+{
+	class Player
+	{
+		private string name;
+		private int age;
+		private int ovr;
+		private float rating;
+		private readonly float index;
+
+		public Player(string name, int age, int ovr, float rating)
+		{
+			this.name=name;
+			this.age=age;
+			this.ovr=ovr;
+			this.rating=rating;
+			this.index=;
+		}
+	}
+}
